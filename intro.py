b398557b1f28452bda7438590dcbe84319344a35
danish <mdk2948@gmail.com>
2023-01-07 18:33:49 +0500

    Python 101

---
+++ intro.py
@@ -0,0 +1,3 @@
+'''This is for the practice of python fundamentals which includes all the things from basics to advance
+The idea is to capture all the programming stuff learnt in this single repo and practice atleast programming 
+30min atleast a day'''
